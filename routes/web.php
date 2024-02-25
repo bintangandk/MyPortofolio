10a521eb606306f47d9fab350978c2a734babd9a
bintangandk <124556644+bintangandk@users.noreply.github.com>
2024-02-25 20:27:48 +0700

    sinkronisasi assets

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('portofolio');
+// });
+
+
+
+Route::group(['namespace' => 'App\Http\Controllers'], function()
+{
+    Route::resource('/', 'PortofolioController');
 });
