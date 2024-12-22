eedfbeb697c7ef88ec7038a58e755f5792edabbb
bintangandk <124556644+bintangandk@users.noreply.github.com>
2024-12-22 19:04:17 +0700

    link to assets

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PortofolioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+Route::get('/', [PortofolioController::class, 'index'])->name('portofolio');
+Route::get('/single', [PortofolioController::class, 'single'])->name('single');
